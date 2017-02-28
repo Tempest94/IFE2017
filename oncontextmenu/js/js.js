93186d8002fd02ee5ec4af664638043299936237
KT <782007058@qq.com>
2017-02-28 11:07:00 +0800

    oncontextmenu 2017-2-28 v1.1

--- oncontextmenu/js/js.js
+++ oncontextmenu/js/js.js
@@ -1,17 +1,20 @@
 window.onload=function(){
 	var floatBox=document.getElementsByClassName("rc")[0];
-	var docX=document.body.clientWidth || document.documentElement.clientWidth;
-	var docY=document.body.clientHeight || document.documentElement.clientHeight;
+	var divBox=document.getElementsByClassName("rc_box")[0];
+	var divX=divBox.offsetLeft+divBox.offsetWidth;
+	var divY=divBox.offsetTop+divBox.offsetHeight;
 	document.oncontextmenu=function(event){
 		var e = event||window.event; 
 		var disX=e.clientX;
 		var disY=e.clientY;
 		e.preventDefault();
-		floatBox.style.display='block';
-		disX=disX>docX-floatBox.offsetWidth?disX-floatBox.offsetWidth:disX;
-		disY=disY>docY-floatBox.offsetHeight?disY-floatBox.offsetHeight:disY;
-		floatBox.style.left=disX+'px';
-		floatBox.style.top=disY+'px';
+		if(disX>divBox.offsetLeft&&disX<divX&&disY>divBox.offsetTop&&disY<divY){
+			floatBox.style.display='block';
+			disX=disX>divX-floatBox.offsetWidth?disX-floatBox.offsetWidth:disX;
+			disY=disY>divY-floatBox.offsetHeight?disY-floatBox.offsetHeight:disY;
+			floatBox.style.left=disX+'px';
+			floatBox.style.top=disY+'px';
+		}
 		return false;
 	}
 	document.onclick=function(){floatBox.style.display='none';};
