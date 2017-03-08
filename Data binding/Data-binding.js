2ee03422c701a57e7b6e138b4d9499a90d32b87f
KT <782007058@qq.com>
2017-03-08 10:59:05 +0800

    Data binding 2017-3-8 v1.

---
+++ Data binding/Data-binding.js
@@ -0,0 +1,59 @@
+// 观察者构造函数
+function Observer(data) {
+    this.data = data;
+    this.walk(data)
+}
+
+var p = Observer.prototype;
+
+// 此函数用于深层次遍历对象的各个属性
+// 采用的是递归的思路
+// 因为我们要为对象的每一个属性绑定getter和setter
+p.walk = function (obj) {
+    var val;
+    for (var key in obj) {
+        // 这里为什么要用hasOwnProperty进行过滤呢？
+    // 因为for...in 循环会把对象原型链上的所有可枚举属性都循环出来
+    // 而我们想要的仅仅是这个对象本身拥有的属性，所以要这么做。
+    if (obj.hasOwnProperty(key)) {
+        val = obj[key];
+
+        // 这里进行判断，如果还没有遍历到最底层，继续new Observer
+        if (typeof val === 'object') {
+                new Observer(val);
+            }
+
+            this.convert(key, val);
+        }
+    }
+};
+
+p.convert = function (key, val) {
+    Object.defineProperty(this.data, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            console.log('你访问了' + key);
+        return val
+	    },
+	    set: function (newVal) {
+	        console.log('你设置了' + key +',新的值为' + newVal);
+	            if (newVal === val) return;
+	            val = newVal
+	        }
+	    })
+};
+var app1 = new Observer({
+  name: 'youngwind',
+  age: 25
+});
+
+var app2 = new Observer({
+  university: 'bupt',
+  major: 'computer'
+});
+var app=new Observer({age:null});
+app1.data.name;
+app.data.age = 100;
+app2.data.university;
+app2.data.major = 'science';           
