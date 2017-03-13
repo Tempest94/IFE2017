c376c88204285b8317829fc947f513dc828888c2
KT <782007058@qq.com>
2017-03-13 14:08:30 +0800

    Data binding-02 v1.1 2017-3-13

--- Data binding-02/Data-binding.js
+++ Data binding-02/Data-binding.js
@@ -1,59 +1,107 @@
-// 观察者构造函数
-function Observer(data) {
-    this.data = data;
-    this.walk(data)
+'use strict'
+
+Observer.prototype ={
+    walk : function (obj) {
+        let val;
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {  
+                val = obj[key];
+                if (typeof val === 'object') {
+                    new Observer(val);
+                }
+
+                this.convert(key, val);
+            }
+            else
+                console.log('No such keys')
+
+        }
+    },
+    convert : function (key, val) {
+        let _this = this;
+        Object.defineProperty(this.data, key, {
+            enumerable: true,
+            configurable: true,
+            get: function () {
+                console.log('你访问了' + key);
+                return val
+            },
+            set: function (newVal) {
+                console.log('你设置了' + key + ', 新的值为' + newVal);
+                if (newVal === val) return;
+                val = newVal
+                _this.emit(key, val, newVal)
+
+            }
+        })
+    },
+    //sub
+    on: function(eventType,handler){
+        var self = this;
+
+        if(!(eventType in self.handlers)){
+            self.handlers[eventType]=[];
+        }
+        self.handlers[eventType].push(handler);
+        return self;
+    },
+
+    // pub
+    emit: function(eventType){
+        console.log(eventType)
+
+        var self = this;
+        if(!self.handlers[eventType]){
+            return;
+        }
+        var handlerArgs = Array.prototype.slice.call(arguments,1);
+        for(var i = 0; i < self.handlers[eventType].length; i++) {
+              self.handlers[eventType][i].apply(self,handlerArgs);
+        }
+        return self;
+    },
+
 }
 
-var p = Observer.prototype;
 
-// 此函数用于深层次遍历对象的各个属性
-// 采用的是递归的思路
-// 因为我们要为对象的每一个属性绑定getter和setter
-p.walk = function (obj) {
-    var val;
-    for (var key in obj) {
-    // 这里为什么要用hasOwnProperty进行过滤呢？
-    // 因为for...in 循环会把对象原型链上的所有可枚举属性都循环出来
-    // 而我们想要的仅仅是这个对象本身拥有的属性，所以要这么做。
-    if (obj.hasOwnProperty(key)) {
-        val = obj[key];
-
-        // 这里进行判断，如果还没有遍历到最底层，继续new Observer
-        if (typeof val === 'object') {
-                new Observer(val);
-            }
-
-            this.convert(key, val);
+let data = {
+    app1:{
+        name: {
+            lastName: 'liang',
+            firstName: 'shaofeng'
+        },
+        age: 25,
+        address: {
+            home: "home address",
+            office: "office address"
         }
+    },
+    app2: {
+         university: 'bupt',
+         age: 28,
+         major: 'computer'
     }
 };
 
-p.convert = function (key, val) {
-    Object.defineProperty(this.data, key, {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-            console.log('你访问了' + key);
-        return val
-	    },
-	    set: function (newVal) {
-	        console.log('你设置了' + key +',新的值为' + newVal);
-	            if (newVal === val) return;
-	            val = newVal
-	        }
-	    })
-};
-var app1 = new Observer({
-  name: 'youngwind',
-  age: 25
+function Observer(data) {
+    this.handlers = {};
+    this.data = data;
+    this.walk(data);
+}
+
+let app1 = new Observer(data.app1);
+let app2 = new Observer(data.app2);
+
+Observer.prototype.$watch=function(attr,callback){
+    this.on(attr,callback);
+}
+
+app1.$watch('age', function(newVal) {
+    console.log(`我的年纪变了，现在已经是：${newVal}岁了`);
 });
 
-var app2 = new Observer({
-  university: 'bupt',
-  major: 'computer'
-});
-var app=new Observer({age:null});
-app1.data.name;
-app.data.age = 100;
-app2.data.university;
-app2.data.major = 'science';           
+//output
+app1.data.name.lastName;
+app1.data.name.firstName = 'lalala';
+
+app1.data.age = 100; // 输出：'我的年纪变了，现在已经是100岁了'
